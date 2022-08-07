7db8d2108c61af5f9858113af15fbe4db1fb6e96
Jason Dhakhwa <jasondhakhwa19@gmail.comm>
2022-08-07 08:35:40 +0545

    first commit

---
+++ products.php
@@ -0,0 +1,36 @@
+<?php include('./include/header.php'); ?>
+
+<!-- For calling products from database -->
+<?php
+$connection = mysqli_connect("localhost", "root", "", "lumanti_web_app");
+if (!$connection) {
+    die("Connection failed..." . mysqli_connect_error());
+}
+$query = "select * from products ";
+$result = mysqli_query($connection, $query);
+
+if (!$result) {
+    die("Query failed..." . mysqli_error($connection));
+};
+?>
+
+<div class="product-item-contains">
+
+    <?php
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo <<<__PRODUCT__
+        <div class='product-item-container'>
+            <img src="./image/$row[image]" class="product-image" alt='image'/>
+            <div class="product-info">
+                <span>Name: $row[Product]</span>
+                <span>Price: $row[Price]</span>
+                <a class="btn" href='product.php?id=$row[ID]&Product=$row[Product]&Price=$row[Price]&image=$row[image]'>Details</a>
+            </div>
+        </div>
+__PRODUCT__;
+    }
+    ?>
+</div>
+</body>
+
+</html>
